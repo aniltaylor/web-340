1a837cba0ae64870c35673f10d62ea67262b0180
Anita L Taylor <98141576+aniltaylor@users.noreply.github.com>
2022-06-08 15:10:58 -0500

    Update team-manager.js

--- week-3/sports-app/team-manager.js
+++ week-3/sports-app/team-manager.js
@@ -1,1 +1,6 @@
-
+function getTeamObjects() {
+    return [
+        {id: 1, name: "", mascot: "", playerCount:"", rank:"", state:""},
+        {id: 2, name: "", mascot: "", PlayerCount: "", rank:"", state:""},
+        {id: 3, name: "", mascot: "", playerCount: "", rank:"", state:""}
+    ];

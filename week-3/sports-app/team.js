82065943fabbb5955a6b6334e68a0d6ac463cdf9
Anita L Taylor <98141576+aniltaylor@users.noreply.github.com>
2022-06-08 13:54:15 -0500

    Update team.js

--- week-3/sports-app/team.js
+++ week-3/sports-app/team.js
@@ -1,1 +1,12 @@
-
+class team {
+  name;
+  mascot;
+  playerCount;
+  constructor(n,m,p) {
+    this.name = n;
+    this.mascot = m;
+    this.playerCount = p;
+  }
+  teamDetails() {
+    return "name: "+this.name +"mascot : "+this.mascot+"playerCount: "+this.playerCount; 
+  }

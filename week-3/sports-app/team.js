008193da5e1cac695905d3307ac64f1efd8a6bcc
Anita L Taylor <98141576+aniltaylor@users.noreply.github.com>
2022-06-12 22:50:35 -0500

    Update team.js

--- week-3/sports-app/team.js
+++ week-3/sports-app/team.js
@@ -1,21 +1,22 @@
-class team {
-  name;
-  mascot;
-  playerCount;
-  constructor(n,m,p) {
-    this.name = n;
-    this.mascot = m;
-    this.playerCount = p;
-  }
-  teamDetails() {
-    return "name: "+this.name +"mascot : "+this.mascot+"playerCount: "+this.playerCount; 
-  }
-  function getTeamObjects() {
-    return [
-        {id: 1, name: "", mascot: "", playerCount:""},
-        {id: 2, name: "", mascot: "", PlayerCount: ""},
-        {id: 3, name: "", mascot: "", playerCount: ""}
-    ];
-module.exports.name = n;
-module.exports.mascot = m;
-module.exports.playerCount = p;
+/*
+============================================
+Title: Assignment 3
+Author: Professor Krasso
+Date: 11 June 2022
+Modified By: <ANITA TAYLOR>
+Description: This program demonstrates the
+use of JavaScript types, values, and
+and variables in an application.
+===========================================
+*/
+class Team {
+    constructor(name, mascot, playerCount, color, rank){
+        this.name = name;
+        this.mascot = mascot;
+        this.playerCount = playerCount;
+        this.color = color;
+        this.rank = rank;
+    }
+}
+
+module.exports = 'Team';

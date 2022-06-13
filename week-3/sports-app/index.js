bab0036a7196d9c5cb4009b22190ab03afe57ec2
Anita L Taylor <98141576+aniltaylor@users.noreply.github.com>
2022-06-12 22:45:34 -0500

    Update index.js

--- week-3/sports-app/index.js
+++ week-3/sports-app/index.js
@@ -8,90 +8,12 @@ Description: This program demonstrates the
 use of JavaScript types, values, and
 and variables in an application.
 ===========================================
-*/
-//Imports dependencies.
+*///Imports dependencies.
 var team = require("./team.js");
 var manager = require("./team-manager.js");
 
 console.log(teams)
 
-//Imports array of team names/getGame string.
+//Imports array of team names and getGame string.
 import * as findTeams from "./team-manager.js";
 import * as getGame from "./team-manager.js";
-
-
-function findTeams(){
-  return teams
-}
-
-//Defines team objects
-const teams = [
-    {
-      name: 'California',
-      mascot: 'Dodgers',
-      playerCount: '33',
-      teamColor: 'red',
-      rank: '6',
-    },
-    {      
-    name: 'NewYork',
-    mascot: 'Yankees',
-    playerCount: '37',
-    teamColor: 'Blue',
-    rank: '9',
-    },
-    {      
-      name: 'Florida',
-      mascot: 'Marlins',
-      playerCount: '35',
-      teamColor: 'brown',
-      rank: '4',
-    },
-    {      
-      name: 'Wisconsin',
-      mascot: 'Badgers',
-      playerCount: '32',
-      teamColor: 'green',
-      rank: '7',
-    },
-    {      
-      name: 'Georgia',
-      mascot: 'Peaches',
-      playerCount: '37',
-      teamColor: 'greengold',
-      rank: '2',
-    }
-  ];
-
-console.log(findTeams());
-
-//This gets team names.
-function findTeam(team){
-    return (name, mascot);
-}
-
-//Moment declaration for date
-var date = moment.js().format('MM DD YY');
-console.log(date);
-
-//return string includes date
-function getGame(string){
-    return (team + mascot + "is playing" + team + mascot + "on" + date + "at 7:00PM CST");
-}
-
-//Exports two functions
-module.exports = 'findTeams()';
-module.exports = 'getGame()';
-
-
-team.jsclass Team {
-    constructor(name, mascot, playerCount, color, rank){
-        this.name = name;
-        this.mascot = mascot;
-        this.playerCount = playerCount;
-        this.color = color;
-        this.rank = rank;
-    }
-}
-
-module.exports = 'Team';

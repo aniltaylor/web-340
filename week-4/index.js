64b520042626a5b41df375e844550a614c85ed77
Anita L Taylor <98141576+aniltaylor@users.noreply.github.com>
2022-06-30 00:13:18 -0500

    Update index.js

--- week-4/index.js
+++ week-4/index.js
@@ -1,1 +1,24 @@
+const express = require("express");
 
+const app = express();
+const port = 3000;
+
+//Static Files Images, JS, CSS Styles
+app.use(express.static("public"));
+app.use("/js", express.static(__dirname + "public/js"));
+app.use("/styles", express.static(__dirname + "public/img"));
+
+//HTML Routes
+app.engine(".html", require("ejs").__express);
+app.set("views", "./views");
+app.set("view engine", "html");
+
+app.get("", (req, res) => {
+  res.render("index");
+});
+
+//Listening on port 3000
+
+app.listen(port, () => {
+  console.log("Application started and listening on port" + port);
+});

261205e76f7e2cc0e0a9ac124a158f80a930f03e
Anita L Taylor <98141576+aniltaylor@users.noreply.github.com>
2022-07-11 22:46:55 -0500

    this is our commit message

---
+++ sports-app/index.js
@@ -0,0 +1,19 @@
+  /*
+============================================
+Title: Assignment 3
+Author: Professor Krasso
+Date: 10 June 2022
+Modified By: <ANITA TAYLOR>
+Description: This program demonstrates the
+use of JavaScript types, values, and
+and variables in an application.
+===========================================
+*///Imports dependencies.
+var team = require("./team.js");
+var manager = require("./team-manager.js");
+
+console.log(teams)
+
+//Imports array of team names and getGame string.
+import * as findTeams from "./team-manager.js";
+import * as getGame from "./team-manager.js";
---
+++ sports-app/team.js
@@ -0,0 +1,22 @@
+/*
+============================================
+Title: Assignment 3
+Author: Professor Krasso
+Date: 11 June 2022
+Modified By: <ANITA TAYLOR>
+Description: This program demonstrates the
+use of JavaScript types, values, and
+and variables in an application.
+===========================================
+*/
+class Team {
+    constructor(name, mascot, playerCount, color, rank){
+        this.name = name;
+        this.mascot = mascot;
+        this.playerCount = playerCount;
+        this.color = color;
+        this.rank = rank;
+    }
+}
+
+module.exports = 'Team';
